2c4f012092f9f2fd8aa01c34e8c0977c3fb45dbf
CMeyer19 <colemeyer95@outlook.com>
2022-03-25 21:46:59 +0200

    Adding initial angular files to repo

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'angular-capacitor-pwa',
+  webDir: 'dist/angular-capacitor-pwa',
+  bundledWebRuntime: false
+};
+
+export default config;
